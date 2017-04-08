77fda46c75d8c7e85355fe9937c5597c8e5a9c2e
JesusPerozo <jesusperozo97@gmail.com>
2017-04-08 01:57:48 -0400

    ahi esta la clase con sus atributos y sus metodos, despues los implementare en el codigo, y tambien hay un archivo que se coleo cuando estaba probando el codigo que hiciste :v.

---
+++ CodigoCesar.h
@@ -0,0 +1,62 @@
+#ifndef CODIGOCESAR_H
+#define CODIGOCESAR_H
+#include <string>
+#include <cstring>
+
+using namespace std;
+class CodigoCesar{
+	
+	public:
+		void setTextoCod(string v, int numero, int tam);
+		void setTextoDecod(string v, int numero, int tam);
+		string getTexto();
+		CodigoCesar(); // el constructor de la clase
+		~CodigoCesar();//el destructor de la clase
+	private:
+		string texto;
+};
+
+
+
+void CodigoCesar::setTextoCod(string tex, int numero, int tam){
+	
+	for(int x=0; x<tam; x++){
+		if(this->texto[x]>64 && this->texto[x]<91){
+			this->texto[x]=this->texto[x]-((numero%26));
+			if(this->texto[x]<65){
+				this->texto[x]=this->texto[x]+26;
+			}
+		}	
+	}
+	this->texto=tex;
+}
+
+void CodigoCesar::setTextoDecod(string tex, int numero, int tam){
+	this->texto=tex;
+	for(int x=0; x<tam; x++){
+		if(this->texto[x]>64 && this->texto[x]<91){
+			this->texto[x]=this->texto[x]+((numero%26));
+			if(this->texto[x]>90){
+				this->texto[x]=this->texto[x]-26;
+			}
+		}
+	}
+}
+
+string CodigoCesar::getTexto(){
+	return this->texto;
+}
+
+
+
+CodigoCesar::CodigoCesar()//Constructor de la clase
+{
+
+}
+
+// class destructor
+CodigoCesar::~CodigoCesar()// destructor de la clase
+{
+
+}
+#endif
